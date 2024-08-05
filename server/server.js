d4f2f30eadea9fe95c2433c462eb6f408c3405e3
unknown <edoyangz@gmail.com>
2024-08-05 21:31:22 +1000

    logging

--- server/server.js
+++ server/server.js
@@ -7,19 +7,35 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => console.log('Connected to database'));
 
-// Allow all origins
+// CORS middleware
 app.use(cors({
-    origin: '*',
+    origin: '*', // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type'], 
 }));
 
+// Log all requests
+app.use((req, res, next) => {
+    console.log(`Received ${req.method} request for ${req.url}`);
+    next();
+});
+
+// Body parser middleware
 app.use(bodyParser.json());
+
+// Test route to check CORS headers
+app.get('/test', (req, res) => {
+    res.json({ message: 'CORS is working!' });
+});
+
+// Application routes
 app.use(routes);
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
