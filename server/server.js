840b442d28e5bffc6666811400be0e50b29caa7b
edoyang <edoyangz@gmail.com>
2025-01-16 18:33:38 +1100

    update server removing the unified topology

--- server/server.js
+++ server/server.js
@@ -1,35 +1,37 @@
-const express = require('express');
-require('dotenv').config();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.once('open', () => console.log('Connected to database'));
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.once("open", () => console.log("Connected to database"));
 
 // CORS middleware
-app.use(cors({
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type'], 
-}));
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 // Log all requests
 app.use((req, res, next) => {
-    console.log(`Received ${req.method} request for ${req.url}`);
-    next();
+  console.log(`Received ${req.method} request for ${req.url}`);
+  next();
 });
 
 // Body parser middleware
 app.use(bodyParser.json());
 
 // Test route to check CORS headers
-app.get('/test', (req, res) => {
-    res.json({ message: 'CORS is working!' });
+app.get("/test", (req, res) => {
+  res.json({ message: "CORS is working!" });
 });
 
 // Application routes
