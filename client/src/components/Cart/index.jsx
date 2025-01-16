68a4439efe2e2dd267a10a24715611f08ae15017
edoyang <edoyangz@gmail.com>
2025-01-16 18:33:14 +1100

    update cart

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,8 +1,17 @@
-import React, { useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { addItem, removeItem, updateItemQuantity, clearCart, toggleCart, closeCart } from '../../redux/cartSlice';
-import './style.scss';
+import React, { useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addItem,
+  removeItem,
+  updateItemQuantity,
+  clearCart,
+  toggleCart,
+  closeCart,
+} from "../../redux/cartSlice";
+import "./style.scss";
+
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.items);
@@ -30,16 +39,58 @@ const Cart = () => {
     if (
       cartRef.current &&
       !cartRef.current.contains(event.target) &&
-      !event.target.closest('.product')
+      !event.target.closest(".product")
     ) {
       dispatch(closeCart());
     }
   };
 
+  const total = cartItems.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+  const stripePublicKey = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
+  const server = import.meta.env.VITE_API_URL;
+
+  const handlePayment = async () => {
+    const stripe = await loadStripe(stripePublicKey);
+    try {
+      const response = await fetch(`${server}/checkout`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ items: cartItems }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const session = await response.json();
+
+      if (!session.id) {
+        throw new Error("Session ID not found");
+      }
+
+      const result = await stripe.redirectToCheckout({
+        sessionId: session.id,
+      });
+
+      if (result.error) {
+        console.error(result.error.message);
+        throw new Error(result.error.message);
+      }
+    } catch (error) {
+      console.error("Payment failed:", error.message);
+      alert("There was an issue with your payment. Please try again.");
+    }
+  };
+
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
@@ -57,19 +108,32 @@ const Cart = () => {
               <p className="item-id">ID: {item.id}</p>
               <div className="buttons">
                 <p className="item-qty">Quantity: {item.quantity}</p>
-                <button onClick={() => handleUpdateQuantity(item.id, item.quantity + 1)}>+</button>
-                <button onClick={() => handleUpdateQuantity(item.id, item.quantity - 1)}>-</button>
-                <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
+                <button
+                  onClick={() =>
+                    handleUpdateQuantity(item.id, item.quantity + 1)
+                  }>
+                  +
+                </button>
+                <button
+                  onClick={() =>
+                    handleUpdateQuantity(item.id, item.quantity - 1)
+                  }>
+                  -
+                </button>
+                <button onClick={() => handleRemoveItem(item.id)}>
+                  Remove
+                </button>
               </div>
             </div>
           ))
         )}
-        {cartItems.length > 0 &&
-          <div className='cart-finalize'>
+        {cartItems.length > 0 && (
+          <div className="cart-finalize">
             <button onClick={handleClearCart}>Clear Cart</button>
-            <Link to="/checkout">Checkout</Link>
+            <button onClick={handlePayment}>Checkout</button>
+            {/* <Link to="/checkout">Checkout</Link> */}
           </div>
-        }
+        )}
       </div>
     </div>
   );
--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,75 +1,80 @@
-import React, { useState } from 'react'
-import './style.scss'
-import { Link } from 'react-router-dom'
-import Cart from '../Cart'
+import React, { useState } from "react";
+import "./style.scss";
+import { Link } from "react-router-dom";
+import Cart from "../Cart";
 
 const Navbar = () => {
-    const [openNavbar, setOpenNavbar] = useState('')
+  const [openNavbar, setOpenNavbar] = useState("");
 
-    const handleHamburger = () => {
-        setOpenNavbar(openNavbar === '' ? 'open' : '')
-    }
+  const handleHamburger = () => {
+    setOpenNavbar(openNavbar === "" ? "open" : "");
+  };
 
   return (
     <div className={`navbar ${openNavbar}`}>
+      <div className="row">
+        <input type="checkbox" name="hamburger" id="hamburger" />
+        <label
+          onClick={handleHamburger}
+          className="hamburger"
+          htmlFor="hamburger">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </label>
+      </div>
 
-        <div className='row'>
-            <input type="checkbox" name="hamburger" id="hamburger" />
-            <label onClick={handleHamburger} className="hamburger" htmlFor="hamburger">
-                <div className="line"></div>
-                <div className="line"></div>
-                <div className="line"></div>
-            </label>
-        </div>
+      <div className="left">
+        <Link to="/">
+          <h1 className="logo">Furniro</h1>
+        </Link>
 
-
-        <div className="left">
-            <Link to="/"><h1 className="logo">Furniro</h1></Link>
-
-            <div className="nav-links">
-                <div className="dropdown">
-                    <p>Products</p>
-                    <div className="dropdown-content">
-                        <a href="#">Chairs</a>
-                        <a href="#">Tables</a>
-                        <a href="#">Beds</a>
-                        <a href="#">Sofas</a>
-                        <a href="#">Cupboards</a>
-                    </div>
-                </div>
-                <div className="dropdown">
-                    <p>Rooms</p>
-                    <div className="dropdown-content">
-                        <a href="#">Living Room</a>
-                        <a href="#">Bedroom</a>
-                        <a href="#">Dining Room</a>
-                        <a href="#">Study Room</a>
-                    </div>
-                </div>
+        <div className="nav-links">
+          <div className="dropdown">
+            <p>Products</p>
+            <div className="dropdown-content">
+              <a href="#">Chairs</a>
+              <a href="#">Tables</a>
+              <a href="#">Beds</a>
+              <a href="#">Sofas</a>
+              <a href="#">Cupboards</a>
             </div>
+          </div>
+          <div className="dropdown">
+            <p>Rooms</p>
+            <div className="dropdown-content">
+              <a href="#">Living Room</a>
+              <a href="#">Bedroom</a>
+              <a href="#">Dining Room</a>
+              <a href="#">Study Room</a>
+            </div>
+          </div>
         </div>
+      </div>
 
-        <div className="search-bar">
-            <label htmlFor="search">
-                <img src="mirror.svg" alt="search" />
-            </label>
-            <input type="text" placeholder="Search for products" id='search' name='search' />
+      <div className="search-bar">
+        <label htmlFor="search">
+          <img src="mirror.svg" alt="search" />
+        </label>
+        <input
+          type="text"
+          placeholder="Search for products"
+          id="search"
+          name="search"
+        />
 
-            <div className="search-result"></div>
-        </div>
+        <div className="search-result"></div>
+      </div>
 
-        <div className="right">
+      <div className="right">
+        <img src="wish.svg" alt="wishlist" />
 
-            <img src="wish.svg" alt="wishlist" />
+        <Cart />
 
-            <Cart />
-
-            <img src="user" alt="user" />
-
-        </div>
-
+        <img src="user.svg" width={24} alt="user" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
